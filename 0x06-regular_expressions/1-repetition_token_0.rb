eddd23522d6d0a1637749eb2d0cabcfba1c3eeb5
Kobbygyan <kobbygyan86@gmail.com>
2023-08-01 17:55:55 -0700

    Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_repetition_token(arg)
-  regex = (/hbt{2,5}n/)
-  if arg =~ regex
-    puts "Match found: #{arg}"
-  else
-    puts "No match found!"
-  end
-end
-
-if ARGV.length != 1
-  puts "Usage: ruby script.rb <input_string>"
-else
-  input_string = ARGV[0]
-  match_repetition_token(input_string)
-end
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
